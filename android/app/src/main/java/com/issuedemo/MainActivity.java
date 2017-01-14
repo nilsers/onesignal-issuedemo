749d9f994352ec4cda714563fee546cd5fb4bf08
Nils-Erik Sirén <nilsers@gmail.com>
2017-01-14 16:11:18 +0200

    react-native

---
+++ android/app/src/main/java/com/issuedemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.issuedemo;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "issuedemo";
+    }
+}
